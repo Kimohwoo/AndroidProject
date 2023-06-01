22737852f449fa36e264a9b7202e9eee52d47fb5
Kimohwoo <kowkow292@naver.com>
2023-06-01 23:26:09 +0900

    weather Retrofit 작업 중 (스프링 연동 관련)

--- app/src/main/java/com/android/andriodproject/retrofit2/NetworkService.kt
+++ app/src/main/java/com/android/andriodproject/retrofit2/NetworkService.kt
@@ -1,11 +1,14 @@
 package com.android.andriodproject.retrofit2
 
+import com.android.andriodproject.User
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface NetworkService {
 
-    @GET("board/")
-    fun getBoard(): Call<BoardModel>
+    @POST("android")
+    fun getLoginResponse(@Body user: User): Call<String>
 
 }
