543cbabfb5147d3c82cdefdd8cf23879120dc367
Kimohwoo <kowkow292@naver.com>
2023-06-02 10:17:56 +0900

    implementation 버전 체크 완료

--- app/src/main/java/com/android/andriodproject/MainActivity.kt
+++ app/src/main/java/com/android/andriodproject/MainActivity.kt
@@ -10,9 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
     }
 
 }
