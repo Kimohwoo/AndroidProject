781f68191b5e818bcba48737b77b0f427d53cc59
lyoun3060 <omega3060@naver.com>
2023-06-07 15:52:29 +0900

    230607 현재 내위치 찍기(완료)

--- app/src/main/java/com/android/andriodproject/GoogleMapsActivity.kt
+++ app/src/main/java/com/android/andriodproject/GoogleMapsActivity.kt
@@ -1,46 +1,182 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.android.andriodproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import androidx.core.content.ContextCompat
+import com.android.andriodproject.PermissionUtils.isPermissionGranted
+import com.android.andriodproject.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.android.andriodproject.databinding.ActivityGoogleMapsBinding
 
-class GoogleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityGoogleMapsBinding
-
+/**
+ * This demo shows how GMS Location can be used to check for changes to the users location.  The
+ * "My Location" button uses GMS Location to set the blue dot representing the users location.
+ * Permission for [Manifest.permission.ACCESS_FINE_LOCATION] and [Manifest.permission.ACCESS_COARSE_LOCATION]
+ * are requested at run time. If either permission is not granted, the Activity is finished with an error message.
+ */
+class GoogleMapsActivity : AppCompatActivity(),
+    OnMyLocationButtonClickListener,
+    OnMyLocationClickListener, OnMapReadyCallback,
+    OnRequestPermissionsResultCallback {
+    /**
+     * [.onRequestPermissionsResult]에서 반환된 이후 요청된 권한이 거부되었는지를 나타내는 플래그입니다.
+     */
+    private var permissionDenied = false
+    private lateinit var map: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityGoogleMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // (자동 생성된 코드) SupportMapFragment 를 가져와서 지도가 준비되면 알림을 받습니다.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        setContentView(R.layout.activity_google_maps)
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
     }
 
-    /** (자동 생성된 코드)
-     * 사용가능한 맵을 조작함. (맵이 준비되면 이 콜백이 실행됨)
-     * 이것으로 마커나 선, 리스터를 추가하거나 표시되는 지역 변경 가능 (디폴트로 호주 시드니를 표시됨)
-     * Google Play 서비스가 설치되어 있지 않으면
-     * SupportMapFragment 안에 서비스를 설치하라는 안내가 표시됨
-     * 이 메서드는 Google Play 서비스를 설치하고 이 앱으로 돌아왔을 때만 실행됨
-     */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
+        googleMap.setOnMyLocationButtonClickListener(this)
+        googleMap.setOnMyLocationClickListener(this)
+        enableMyLocation()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    /**
+     * 세부 위치 권한이 허용된 경우 내 위치 레이어를 활성화합니다.
+     */
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+
+        // 현재 위치 레이어를 활성화하기 위해 권한이 허용되었는지 확인합니다.
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // 권한이 허용된 경우, 내 위치 레이어를 활성화합니다.
+            map.isMyLocationEnabled = true
+            return
+        }
+
+        // 2. 권한을 요청할 때 권한 이유 다이얼로그를 표시해야 하는지 확인합니다.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            PermissionUtils.RationaleDialog.newInstance(
+                LOCATION_PERMISSION_REQUEST_CODE, true
+            ).show(supportFragmentManager, "dialog")
+            return
+        }
+
+        // 3. 그렇지 않은 경우 권한 요청
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
+            .show()
+        // 이벤트를 소비하지 않고 기본 동작이 발생하도록 false를 반환합니다.
+        // (카메라가 사용자의 현재 위치로 애니메이션됩니다).
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+            )
+            return
+        }
+
+        if (isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            // 권한이 허용된 경우 내 위치 레이어를 활성화합니다.
+            enableMyLocation()
+        } else {
+            // 권한이 거부되었습니다. 오류 메시지를 표시합니다.
+            // Fragment가 다시 시작될 때 누락된 권한 오류 대화상자를 표시합니다.
+            permissionDenied = true
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (permissionDenied) {
+            // 권한이 허용되지 않았으므로 오류 대화상자를 표시합니다.
+            showMissingPermissionError()
+            permissionDenied = false
+        }
+    }
+
+    /**
+     * 위치 권한이 없음을 설명하는 오류 메시지가 포함된 대화상자를 표시합니다.
+     */
+    private fun showMissingPermissionError() {
+        newInstance(true).show(supportFragmentManager, "dialog")
+    }
+
+    companion object {
+        /**
+         * 위치 권한 요청에 대한 요청 코드입니다.
+         *
+         * @see .onRequestPermissionsResult
+         */
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 }
