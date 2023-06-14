12dc97968930a795aa6947ac56a81faa7bb3faf8
lyoun3060 <omega3060@naver.com>
2023-06-14 11:58:34 +0900

    230614 수정본 업로드

--- app/src/main/java/com/android/andriodproject/MainActivity.kt
+++ app/src/main/java/com/android/andriodproject/MainActivity.kt
@@ -2,27 +2,20 @@ package com.android.andriodproject
 
 import android.location.LocationManager
 import android.os.Bundle
-<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity
-=======
->>>>>>> master
 import com.android.andriodproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-<<<<<<< HEAD
     private val locationManager: LocationManager? = null
     private val REQUEST_CODE_LOCATION = 2
 
     //    var id: String = ""
-//    var password: String = ""
-=======
->>>>>>> master
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-<<<<<<< HEAD
 
 //        binding.btn.setOnClickListener {
 //            id = binding.id.text.toString()
@@ -59,13 +52,5 @@ class MainActivity : AppCompatActivity() {
 //
 //
 //    }
-
-
-=======
-
-
-
-    }
->>>>>>> master
 
 }
