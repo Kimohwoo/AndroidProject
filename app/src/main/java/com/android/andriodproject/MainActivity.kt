6c67e9957d4a26d96de3e99206a21a3e1826e56d
lyoun3060 <omega3060@naver.com>
2023-06-20 10:27:22 +0900

    230619 메인작업중,, 사진,갤러리 잘안됨

--- app/src/main/java/com/android/andriodproject/MainActivity.kt
+++ app/src/main/java/com/android/andriodproject/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.android.andriodproject
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build.VERSION_CODES.M
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.android.andriodproject.databinding.ActivityMainBinding
 
 
@@ -17,6 +23,10 @@ class MainActivity : AppCompatActivity() {
     private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim) }
     private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim) }
     private var clicked = false
+    private val CAMERA_PERMISSION_REQUEST_CODE = 1001
+    private val CAMERA_USAGE_REQUEST_CODE = 1002
+    private val CAMERA_STORAGE_REQUEST_CODE = 1003
+
 
     companion object {
         const val uid = "abcdefg"
@@ -33,9 +43,17 @@ class MainActivity : AppCompatActivity() {
         binding.addBtn.setOnClickListener {
             onAddButtonClicked()
         }
+
         binding.cameraBtn.setOnClickListener {
             Toast.makeText(this, "플로팅1", Toast.LENGTH_SHORT).show()
+            if(cameraPermission() && cameraStoragePermission()){
+                openCamera()
+            }else{
+                requestPermissions()
+            }
         }
+
+
         binding.galleryBtn.setOnClickListener {
             Toast.makeText(this, "플로팅2", Toast.LENGTH_SHORT).show()
         }
@@ -46,6 +64,62 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    //카메라 권한이 있는지 여부
+    private fun cameraPermission() : Boolean{
+        val cameraPermission = Manifest.permission.CAMERA
+        val result = ContextCompat.checkSelfPermission(this, cameraPermission)
+        return result == PERMISSION_GRANTED //<- 권한 결과를 반환하는데 true/flase를 반환
+    }
+
+    //카메라 저장할때 권한이 있는지 여부
+    private fun cameraStoragePermission(): Boolean {
+        val storagePermission = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        val result = ContextCompat.checkSelfPermission(this, storagePermission)
+        return result == PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        val cameraPermission = Manifest.permission.CAMERA
+        val storagePermission = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        val permissionList = arrayOf(cameraPermission, storagePermission)
+
+        ActivityCompat.requestPermissions(this, permissionList, CAMERA_PERMISSION_REQUEST_CODE)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 카메라 권한 허용
+                if (hasStoragePermission()) {
+                    openCamera()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        STORAGE_PERMISSION_REQUEST_CODE
+                    )
+                }
+            } else {
+                // 카메라 권한 거부
+                // 필요한 처리를 수행하세요.
+            }
+        } else if (requestCode == STORAGE_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 저장소 권한 허용
+                openCamera()
+            } else {
+                // 저장소 권한 거부
+                // 필요한 처리를 수행하세요.
+            }
+        }
+    }
+
 
     private fun onAddButtonClicked() {
         setVisibility(clicked)
